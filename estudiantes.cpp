00f5c65b15e4bf17d8ac0bfa813e2d59a5b3e078
Manuel Bravo <mbravo@gmx.es>
2021-07-12 10:18:29 +0200

    Agregar archivos de proyecto.

---
+++ estudiantes.cpp
@@ -0,0 +1,16 @@
+//5. Realice un programa que le permita introducir 5 nombres de estudiantes,
+//luego ingrese las notas del primer parcial segundo parcial y
+//2 actividades en clase y si la suma de estos es menor de 21
+//muestre un mensaje que diga lamentablemente el estudiante NOMBRE no tiene derecho a examen final si
+//la suma es mayor que 20 pero menor que 28 el mensaje deberá de decir necesita esforzarse para aprobar el curso y
+//si la suma es mayor de 28 el mensaje será nos vemos el próximo semestre en otro curso
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    
+}
+
